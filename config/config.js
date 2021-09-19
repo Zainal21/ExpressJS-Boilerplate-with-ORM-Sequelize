6fa6c318544fd06095195a3d0e316661de67cd96
Muhamad Zainal Arifin <zainalarifin080718@gmail.com>
2021-09-19 11:22:53 +0700

    chore(depedency) : update depedency

--- config/config.js
+++ config/config.js
@@ -1,27 +1,29 @@
-require('dotenv').config();
+/** @format */
+
+require("dotenv").config();
 // set var obj db
-const {DB_NAME, DB_USERNAME, DB_PASSWORD, DB_HOSTNAME} = process.env
+const { DB_NAME, DB_USERNAME, DB_PASSWORD, DB_HOSTNAME } = process.env;
 
 module.exports = {
-  "development": {
-    "username": DB_USERNAME,
-    "password": DB_PASSWORD,
-    "database": DB_NAME,
-    "host": DB_HOSTNAME,
-    "dialect": "mysql"
-  },
-  "test": {
-    "username": DB_USERNAME,
-    "password": DB_PASSWORD,
-    "database":DB_NAME ,
-    "host": DB_HOSTNAME,
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": DB_USERNAME,
-    "password": DB_PASSWORD,
-    "database": DB_NAME,
-    "host": DB_HOSTNAME,
-    "dialect": "mysql"
-  }
-}
+	development: {
+		username: DB_USERNAME,
+		password: DB_PASSWORD,
+		database: DB_NAME,
+		host: DB_HOSTNAME,
+		dialect: "mysql",
+	},
+	test: {
+		username: DB_USERNAME,
+		password: DB_PASSWORD,
+		database: DB_NAME,
+		host: DB_HOSTNAME,
+		dialect: "mysql",
+	},
+	production: {
+		username: DB_USERNAME,
+		password: DB_PASSWORD,
+		database: DB_NAME,
+		host: DB_HOSTNAME,
+		dialect: "mysql",
+	},
+};
