1cf1d623c35c4212627757fa8f4373b21e71700f
Zainal21 <Zainalarifin080718@gmail.com>
2023-02-09 19:57:15 +0700

    chore : change response formmater in helper

--- helpers/ResponseFormatter.js
+++ helpers/ResponseFormatter.js
@@ -1,31 +1,43 @@
 /** @format */
 module.exports = {
-	ResponseFormatter: ({
-		action,
-		res_body
-	}, res) => {
-		switch (action) {
-			case "SUCCESS":
-				res.status(200).json({
-					status: 200,
-					success: true,
-					response: res_body,
-				});
-				break;
-			case "BAD_REQUEST":
-				res.status(400).json({
-					status: 400,
-					success: false,
-					response: res_body,
-				});
-				break;
-			default:
-				res.status(404).json({
-					status: 404,
-					success: false,
-					response: "Data not found",
-				});
-				break;
-		}
-	},
-}
+  ResponseFormatter: ({ action, responseBody }, res) => {
+    switch (action) {
+      case "SUCCESS":
+        res.status(200).json({
+          message: "Request successfully",
+          status: 200,
+          success: true,
+          data: responseBody,
+          timestamp: Date.now(),
+        });
+        break;
+      case "BAD_REQUEST":
+        res.status(400).json({
+          message: "Baq Request",
+          status: 400,
+          success: false,
+          data: responseBody,
+          timestamp: Date.now(),
+        });
+        break;
+      case "SERVER_ERROR":
+        res.status(500).json({
+          message: "Internal Server Error",
+          status: 500,
+          success: false,
+          data: responseBody,
+          timestamp: Date.now(),
+        });
+        break;
+      default:
+        res.status(404).json({
+          message: "Data not found",
+          status: 404,
+          success: false,
+          data: null,
+          timestamp: Date.now(),
+        });
+        break;
+    }
+  },
+};
--- middleware/AuthMiddleware.js
+++ middleware/AuthMiddleware.js
@@ -1,22 +1,28 @@
 module.exports = (req, res, next) => {
   const HeaderAuth = req.headers.authorization;
-  var accessTokenSecret = process.env.JWT_SECRET_KEY
+  var accessTokenSecret = process.env.JWT_SECRET_KEY;
   if (HeaderAuth) {
-    const token = HeaderAuth.split(' ')[1]
+    const token = HeaderAuth.split(" ")[1];
     jwt.verify(token, accessTokenSecret, (err, user) => {
       if (err) {
-        return res.json({
-          message: 'Forbidden',
-          code: 403
-        })
+        return res.status(403).json({
+          message: "Route Access Forbidden",
+          status: 403,
+          success: false,
+          data: null,
+          timestamp: Date.now(),
+        });
       }
       req.user = user;
       next();
-    })
+    });
   } else {
-    res.json({
-      message: 'Unauthorized',
-      code: 401
-    })
+    res.status(401).json({
+      message: "Unauthorized",
+      status: 401,
+      success: false,
+      data: null,
+      timestamp: Date.now(),
+    });
   }
-}
+};
