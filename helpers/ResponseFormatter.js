43512ff828a6bf8889a1d40172c3f596dce1633c
Zainal21 <Zainalarifin080718@gmail.com>
2023-02-10 10:26:13 +0700

    chore : change response message helper

--- helpers/ResponseFormatter.js
+++ helpers/ResponseFormatter.js
@@ -1,10 +1,10 @@
 /** @format */
 module.exports = {
-  ResponseFormatter: ({ action, responseBody }, res) => {
+  ResponseHelper: ({ action, responseBody, message }, res) => {
     switch (action) {
       case "SUCCESS":
         res.status(200).json({
-          message: "Request successfully",
+          message: message || "Request successfully",
           status: 200,
           success: true,
           data: responseBody,
@@ -13,7 +13,7 @@ module.exports = {
         break;
       case "BAD_REQUEST":
         res.status(400).json({
-          message: "Baq Request",
+          message: message || "Baq Request",
           status: 400,
           success: false,
           data: responseBody,
@@ -22,16 +22,25 @@ module.exports = {
         break;
       case "SERVER_ERROR":
         res.status(500).json({
-          message: "Internal Server Error",
+          message: message || "Internal Server Error",
           status: 500,
           success: false,
           data: responseBody,
           timestamp: Date.now(),
         });
         break;
+      case "NOTFOUND":
+        res.status(404).json({
+          message: message || "Data not found",
+          status: 404,
+          success: false,
+          data: null,
+          timestamp: Date.now(),
+        });
+        break;
       default:
         res.status(404).json({
-          message: "Data not found",
+          message: message || "Data not found",
           status: 404,
           success: false,
           data: null,
